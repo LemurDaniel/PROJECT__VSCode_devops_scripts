6bc66323f8835df6bbd6f575517d737a9441b180
LemurDaniel <Landau.Daniel.1998@gmail.com>
2023-04-16 21:28:29 +0200

    test github api

--- extension.js
+++ extension.js
@@ -2,6 +2,9 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+const github = require('./lib/github/activate.js')
+const SecretData = require('./lib/utils/SecretData.js')
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -10,18 +13,23 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
+	SecretData.init(context)
+	github.activate()
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "devops-scripts" is now active!');
+	console.log('Congratulations, your extension "devopsscripts" is now active!');
+
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('devops-scripts.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('devopsscripts.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DevOps Scripts!');
+		vscode.window.showInformationMessage('Hello World from DevOpsScripts!');
 	});
 
 	context.subscriptions.push(disposable);

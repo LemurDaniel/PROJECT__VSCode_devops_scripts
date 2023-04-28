0d59c60b0adcc037c612e628fa263f0baa730b7d
LemurDaniel <Landau.Daniel.1998@gmail.com>
2023-04-28 20:46:25 +0200

    Add ability to switch branch on viewed Repository

--- extension.js
+++ extension.js
@@ -2,19 +2,22 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
 const devops = require('./lib/activate.js')
+const output = require('./lib/output')
 const SecretData = require('./lib/utils/SecretData.js')
 
+
+const RepositoryView = require('./lib/repositoryView')
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
 
 	SecretData.init(context)
 	devops.activate(context)
-	
+
 }
 
 // This method is called when your extension is deactivated

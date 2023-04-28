58444b61debbf6d1bb4921f00d2f7da6cea12360
LemurDaniel <Landau.Daniel.1998@gmail.com>
2023-04-28 19:05:36 +0200

    file-explorer-like view on selected downloaded repository For quickly looking which files are contained in the repository with opening it

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-
+const vscode = require('vscode')
 const devops = require('./lib/activate.js')
 const SecretData = require('./lib/utils/SecretData.js')
 
